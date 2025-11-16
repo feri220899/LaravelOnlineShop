2f01b064ea30944c56cec11903eb97c6d6393bd6
feri220899 <61813824+feri220899@users.noreply.github.com>
2025-11-16 13:21:17 +0700

    laravel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,5 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.url')) {
             URL::forceRootUrl(config('app.url'));
         }
-        Livewire::component('products-control', ProductsControl::class);
     }
 }
